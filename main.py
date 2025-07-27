818dbb1fbb111f25dc3afa1b5379a5fdb82d64c3
Simón Valdés <s.valdesoliva@icloud.com>
2025-07-27 16:20:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def renombrador(archivo):
     stream = chat(
         model=modelo,
         messages=[{'role': 'user',
-                   'content': f'Propón un único nombre técnico y estructurado para titular un archivo PDF académico, basado en el siguiente extracto: {text}. El nombre debe ser claro, conciso y específico, similar a “Clase 1.1 Cálculo Multivariable”. Usa un estilo formal, ordenado y orientado a cursos o documentos académicos. No incluyas explicaciones ni comillas. Entrega solo el nombre, en una línea. No seas estupido, no cometas errores'}],
+                   'content': f'Propón un único nombre técnico y estructurado para titular un archivo PDF académico, basado en el siguiente extracto: {text}. El nombre debe ser claro, conciso y específico, similar a “Clase 1.1 Cálculo”. Usa un estilo formal, ordenado y orientado a cursos o documentos académicos. No incluyas explicaciones ni comillas. Entrega solo el nombre, en una línea. No seas estupido, no cometas errores'}],
         stream=True,
     )
 
@@ -36,7 +36,7 @@ def seleccion(archivo):
     stream = chat(
         model=modelo,
         messages=[{'role': 'user',
-                   'content': f'Elige el mejor título de los siguientes 25, aquel que describa con mayor precisión el contenido del archivo y que, al mismo tiempo, facilite su organización en el computador. El archivo es: {archivo}. Los títulos son: {lista}. Entrega únicamente el título seleccionado, sin explicación, en una sola línea. No uses comillas ni texto adicional. No seas estupido, no cometas errores.'}],
+                   'content': f'Elige el mejor título de la siguiente lista, aquel que describa con mayor precisión el contenido del archivo y que, al mismo tiempo, facilite su organización en el computador. El archivo es: {archivo}. Los títulos son: {lista}. Entrega únicamente el título seleccionado, sin explicación, en una sola línea. No uses comillas ni texto adicional. No seas estupido, no cometas errores.'}],
         stream=True,
     )
 
@@ -48,7 +48,6 @@ def seleccion(archivo):
 
 
 
-print(seleccion("/Users/svaldes/Desktop/Universidad/2025-1/MAT203 Cálculo Multivariables/Clases/Clase 22 rotacional y divergencia.pdf"))
 
 
 
